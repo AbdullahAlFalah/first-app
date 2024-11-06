62e54fd43c2fabc85e9fc4cf0d3a5f774274ba9f
AbdullahAlFalah <salem908mk@gmail.com>
2024-11-06 19:24:04 +0200

    Screenshot1notdone

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import UserProvider from "@/hooks/UserContext";
+import Screenshot from "@/components/Functional/Screenshot";
 
 export default function RootLayout() {
   return (
     
-    <UserProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-        <Stack.Screen name="+not-found"  options={{ title: 'Page not found...', headerShadowVisible: false, headerTintColor: '#fff', headerStyle: {backgroundColor: '#25292e'} }} />
-      </Stack>
-    </UserProvider>
+    <GestureHandlerRootView>
+        <UserProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+            <Stack.Screen name="+not-found"  options={{ title: 'Page not found...', headerShadowVisible: false, headerTintColor: '#fff', headerStyle: {backgroundColor: '#25292e'} }} />
+          </Stack>
+        </UserProvider>
+    </GestureHandlerRootView>
 
   );
-}
+};
 
