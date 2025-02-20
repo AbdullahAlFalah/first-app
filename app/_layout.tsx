ed1b9209d90aa85dee28a939fdef256ca981f970
AbdullahAlFalah <salem908mk@gmail.com>
2025-02-20 15:55:57 +0200

    IconChange_StatusBarOverrideVIPduetoexporouterbug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,27 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useEffect } from "react";
 
 import UserProvider from "@/hooks/UserContext";
 
 export default function RootLayout() {
+
+
+
   return (
     
-    <GestureHandlerRootView>
-      <UserProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
-          <Stack.Screen name="+not-found"  options={{ title: 'Page not found...', headerShadowVisible: false, headerTintColor: '#fff', headerStyle: {backgroundColor: '#25292e'} }} />
-        </Stack>
-      </UserProvider>
-    </GestureHandlerRootView>
-
+      <GestureHandlerRootView>
+        <UserProvider>
+          <StatusBar style="light" />
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+            <Stack.Screen name="+not-found"  options={{ title: 'Page not found...', headerShadowVisible: false, headerTintColor: '#fff', headerStyle: {backgroundColor: '#25292e'} }} />
+          </Stack>         
+        </UserProvider>
+      </GestureHandlerRootView>
+        
   );
+  
 };
 
