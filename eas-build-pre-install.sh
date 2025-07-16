3a32dedc3297ec1ef7c76762726409d0feae951b
AbdullahAlFalah <salem908mk@gmail.com>
2025-07-16 19:11:58 +0300

    fix_2_double_quotation

--- eas-build-pre-install.sh
+++ eas-build-pre-install.sh
@@ -10,7 +10,7 @@ fi
 echo "✅ GOOGLE_SERVICES_JSON is set. Length: ${#GOOGLE_SERVICES_JSON}"
 echo "${GOOGLE_SERVICES_JSON:0:50}..."  # Print the first 50 characters for sanity check
 
-echo $GOOGLE_SERVICES_JSON | base64 -d > android/app/google-services.json
+echo "$GOOGLE_SERVICES_JSON" | base64 -d > android/app/google-services.json
 
 if [ ! -s google-services.json ]; then
   echo "❌ Error: Failed to create google-services.json from GOOGLE_SERVICES_JSON."
